f1bf031b4e513e53b6e52fca8b30e509621aa79c
huangw <bwilling@163.com>
2019-11-09 15:37:43 +0800

    旋转数组

--- lc-java/src/Solution.java
+++ lc-java/src/Solution.java
@@ -1,4 +1,50 @@
+import java.util.Arrays;
+
 public class Solution {
+
+    /**
+     * 189. 旋转数组
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * @param nums
+     * @param k
+     */
+    public static void rotate(int[] nums,int k){
+        //1.旋转即将数组最后的元素移动到数组的最前面
+        //2.移动 k 个元素，也就是将前面 length-k个元素移动到数组最后
+        //3.创建一个嵌套循环，内循环将数组第一个元素移动至末尾，外循环为需要被移动的元素个数
+        //4. 空间复杂度为 O(1) 时间复杂度为0(n^2)
+        if(nums.length<2||k==0)
+            return;
+        if(k>nums.length){
+            k = k%nums.length;
+        }
+        int tmp = 0;
+        for (int i=0;i<nums.length-k;i++){
+            for(int j=0;j<nums.length-1;j++){
+                tmp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = tmp;
+            }
+        }
+    }
+
+    public static void rotate2(int[] nums,int k){
+        //1.旋转即将数组最后的元素移动到数组的最前面
+        //2.复制一个新的数组，用两个并列循环分别处理 k 个元素的赋值问题
+        if(nums.length<2||k==0)
+            return;
+        if(k>nums.length){
+            k = k%nums.length;
+        }
+        int[] arrNums = Arrays.copyOf(nums,nums.length);
+        int j= 0;
+        for(int i=0;i<arrNums.length-k;i++){
+            nums[k+i] = arrNums[j++];
+        }
+        for(int i=0;i<k;i++){
+            nums[i] = arrNums[arrNums.length-k+i];
+        }
+    }
 
     /**
      * 删除排序数组中的重复项
@@ -20,7 +66,11 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println(removeDuplicates(nums));
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate2(nums,2);
+        for(int n:nums){
+            System.out.print(n);
+        }
+//        System.out.println(removeDuplicates(nums));
     }
 }
